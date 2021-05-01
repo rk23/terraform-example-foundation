e0a9e9da6839328778a4a0f462d22cf2388ddc70
Reed Kinning <rkinning@nuvalence.io>
2021-04-30 22:41:42 -0400

    add org locals for linking

---
+++ org_locals.tf
@@ -0,0 +1,19 @@
+locals {
+  billing_account            = data.terraform_remote_state.bootstrap.outputs.billing_account
+  common_folder              = data.terraform_remote_state.bootstrap.outputs.common_folder
+  default_region             = data.terraform_remote_state.bootstrap.outputs.default_region
+  domain                     = data.terraform_remote_state.bootstrap.outputs.domain
+  org_basename               = data.terraform_remote_state.bootstrap.outputs.org_basename
+  org_shortname              = data.terraform_remote_state.bootstrap.outputs.org_shortname
+  terraform_service_account  = data.terraform_remote_state.bootstrap.outputs.terraform_sa_email
+  terraform_state_project_id = data.terraform_remote_state.bootstrap.outputs.terraform_state_project_id
+  org_id                     = var.org_id
+}
+
+data "terraform_remote_state" "bootstrap" {
+  backend = "gcs"
+  config = {
+    bucket = "bkt-kvi-gcp-foundation-tfstate"
+    prefix = "terraform/bootstrap/state"
+  }
+}
